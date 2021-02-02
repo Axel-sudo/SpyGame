0caed575f57516e4e544c79e3957e65c07626323
emilie <emilieyapoo@gmail.com>
2021-02-02 16:12:40 +0100

    new activites android

---
+++ app/src/main/java/com/example/spygame/AgentScreen.java
@@ -0,0 +1,14 @@
+package com.example.spygame;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class AgentScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_agent_screen);
+    }
+}
