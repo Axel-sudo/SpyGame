a7c0ca06e3561f0488c1f0a8d1033064c6ae7b07
emilie <emilieyapoo@gmail.com>
2021-02-09 18:24:23 +0100

    Link between activities + Exit logo

--- app/src/main/java/com/example/spygame/MainActivity.java
+++ app/src/main/java/com/example/spygame/MainActivity.java
@@ -4,19 +4,22 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.widget.Button;
 import android.content.Intent;
 import android.view.View;
-
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
-    public Button button;
+    public Button button,setting;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        button = (Button) findViewById(R.id.button3);
+        button =(Button)findViewById(R.id.button3);
+        setting =(Button)findViewById(R.id.button5);
+
+
 
         button.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -24,6 +27,16 @@ public class MainActivity extends AppCompatActivity {
 
                 Intent intent = new Intent(MainActivity.this,PartyConfiguration.class);
                 startActivity(intent);
+
+            }
+        });
+
+        setting.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+             Intent intent2 = new Intent(MainActivity.this,Settings.class);
+             startActivity(intent2);
 
             }
         });
--- app/src/main/java/com/example/spygame/PartyConfiguration.java
+++ app/src/main/java/com/example/spygame/PartyConfiguration.java
@@ -6,6 +6,9 @@ import android.os.Bundle;
 
 public class PartyConfiguration extends AppCompatActivity {
 
+
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
