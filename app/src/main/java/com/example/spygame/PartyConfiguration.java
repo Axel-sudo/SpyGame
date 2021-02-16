c836f8814027a17ab28ce6fa0deb10e2f88e03ec
emilie <emilieyapoo@gmail.com>
2021-02-16 16:25:31 +0100

    button connection Game screen

--- app/src/main/java/com/example/spygame/PartyConfiguration.java
+++ app/src/main/java/com/example/spygame/PartyConfiguration.java
@@ -2,16 +2,29 @@ package com.example.spygame;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class PartyConfiguration extends AppCompatActivity {
 
-
-
+    public Button buttonParty;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_party_configuration);
+
+        buttonParty=(Button)findViewById(R.id.joinGame);
+
+        buttonParty.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(PartyConfiguration.this,GameBoardActivity.class);
+                startActivity(intent);
+            }
+        });
+
     }
 }
